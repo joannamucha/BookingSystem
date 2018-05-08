56efe6c2840280d6d150b08f5427b8b8dba74259
Maria Asenova <mariaasenova22@gmail.com>
2018-05-08 10:54:15 +0200

    tyt

--- book.js
+++ book.js
@@ -1,9 +1,11 @@
 var tables = document.querySelectorAll('article.restaurant, article.terrace');
+
 console.log(tables);
 
 for ( var i = 0; i < tables.length; i++ ) (function(i){
   tables[i].onclick = function() {
       console.log('I am clicked');
-      //take the box and add class of block
+      var tableStatus = document.querySelector('tableStatus');
+      tableStatus.classList.add('tableStatusDisplay');
   }
 })(i);
