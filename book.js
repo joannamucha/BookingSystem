3a226141f71b0bc13e7a8b11ce137828adf6d112
Maria Asenova <mariaasenova22@gmail.com>
2018-05-08 10:39:33 +0200

    last

--- book.js
+++ book.js
@@ -4,5 +4,6 @@ console.log(tables);
 for ( var i = 0; i < tables.length; i++ ) (function(i){
   tables[i].onclick = function() {
       console.log('I am clicked');
+      //take the box and add class of block
   }
 })(i);
